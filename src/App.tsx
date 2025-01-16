9e6db213a390b8e9fd486742749c34da007f96a7
Esther299 <esthergc_2@hotmail.com>
2025-01-16 21:24:25 +0100

    Cleaned up unused files

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
 import { NavBar } from "./components/NavBar";
 import Home from "./pages/Home";
 import { Profile } from "./pages/Profile";
-import { Search } from "./pages/Search";
 import MetMuseumPage from "./pages/MetMuseumSearch";
 import ChicagoMuseumPage from "./pages/ChicagoMuseumSearch";
 import AllProviders from "./context/AllProviders";
@@ -27,7 +26,6 @@ const AppContent: React.FC = () => {
           <>
             <Route path="/home" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/search" element={<Search />} />
             <Route path="/met" element={<MetMuseumPage />} />
             <Route path="/chicago" element={<ChicagoMuseumPage />} />
             <Route path="/exhibitions" element={<Exhibitions />} />
