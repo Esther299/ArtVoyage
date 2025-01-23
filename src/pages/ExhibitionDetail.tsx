c0d082960d7581d5fdd69b3807b6e3dcd9f0d8ca
Esther299 <esthergc_2@hotmail.com>
2025-01-23 21:58:05 +0100

    Corrected exhibition display format, added missing functionality and cleaned up code

--- src/pages/ExhibitionDetail.tsx
+++ src/pages/ExhibitionDetail.tsx
@@ -3,8 +3,6 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useExhibitions } from "../context/ExhibitionContext";
 import { Exhibition } from "../types/types";
 import ExhibitionCard from "../components/ExhibitionCard";
-import DeleteArtworkModal from "../components/DeleteArtworkModal";
-import EditExhibitionModal from "../components/EditExhibitionModal";
 
 const ExhibitionDetail: React.FC = () => {
   const { exhibitionId } = useParams();
@@ -15,27 +13,10 @@ const ExhibitionDetail: React.FC = () => {
     editExhibition,
     deleteArtworkFromExhibition,
     deleteExhibition,
-    addArtworkToExhibition,
   } = useExhibitions();
   const [exhibition, setExhibition] = useState<Exhibition | null>(null);
-  const [newArtworkTitle, setNewArtworkTitle] = useState("");
-  const navigate = useNavigate();
-
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [showEditModal, setShowEditModal] = useState(false);
-
-  const [selectedExhibitionId, setSelectedExhibitionId] = useState<
-    string | null
-  >(null);
-  const [selectedArtworkId, setSelectedArtworkId] = useState<number | null>(
-    null
-  );
-
-  const [editingExhibition, setEditingExhibition] = useState<Exhibition | null>(
-    null
-  );
-
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (exhibitionId) {
@@ -48,6 +29,21 @@ const ExhibitionDetail: React.FC = () => {
     }
   }, [exhibitionId, exhibitions, navigate]);
 
+  const handleAddArtwork = () => {
+    navigate("/search");
+  };
+
+  const handleEditExhibition = useCallback(
+    (
+      exhibitionId: string,
+      updatedFields: { name: string; startDate: string; endDate: string }
+    ) => {
+      editExhibition(exhibitionId, updatedFields);
+      setSuccessMessage("Exhibition editted successfully.");
+    },
+    [deleteArtworkFromExhibition]
+  );
+
   const handleDeleteExhibition = useCallback(
     (exhibitionId: string) => {
       deleteExhibition(exhibitionId);
@@ -56,10 +52,6 @@ const ExhibitionDetail: React.FC = () => {
     [deleteExhibition]
   );
 
-  const handleAddArtwork = () => {
-    navigate("/search");
-  };
-
   const handleDeleteArtwork = useCallback(
     (exhibitionId: string, artworkId: number) => {
       deleteArtworkFromExhibition(exhibitionId, artworkId);
@@ -67,32 +59,6 @@ const ExhibitionDetail: React.FC = () => {
     },
     [deleteArtworkFromExhibition]
   );
-
-  const handleShowDeleteModal = useCallback(
-    (exhibitionId: string, artworkId: number) => {
-      setSelectedExhibitionId(exhibitionId);
-      setSelectedArtworkId(artworkId);
-      setShowDeleteModal(true);
-    },
-    []
-  );
-
-  const handleShowEditModal = useCallback((exhibition: Exhibition) => {
-    setEditingExhibition(exhibition);
-    setShowEditModal(true);
-  }, []);
-
-  const closeDeleteModal = () => {
-    setShowDeleteModal(false);
-    setSelectedExhibitionId(null);
-    setSelectedArtworkId(null);
-  };
-
-  const closeEditModal = () => {
-    setShowEditModal(false);
-    setEditingExhibition(null);
-    setSuccessMessage("Exhibition edited successfully.");
-  };
 
   useEffect(() => {
     if (successMessage) {
@@ -124,31 +90,10 @@ const ExhibitionDetail: React.FC = () => {
       <ExhibitionCard
         exhibition={exhibition}
         handleDeleteExhibition={handleDeleteExhibition}
-        handleShowModal={handleShowDeleteModal}
-        handleShowEditModal={handleShowEditModal}
+        handleDeleteArtwork={handleDeleteArtwork}
+        handleEditExhibition={handleEditExhibition}
         handleAddArtwork={handleAddArtwork}
       />
-
-      {showEditModal && editingExhibition && (
-        <EditExhibitionModal
-          show={showEditModal}
-          handleClose={closeEditModal}
-          handleEditExhibition={editExhibition}
-          exhibition={editingExhibition}
-        />
-      )}
-
-      {showDeleteModal &&
-        selectedExhibitionId &&
-        selectedArtworkId !== null && (
-          <DeleteArtworkModal
-            show={showDeleteModal}
-            handleClose={closeDeleteModal}
-            handleDeleteArtwork={handleDeleteArtwork}
-            selectedExhibitionId={selectedExhibitionId}
-            selectedArtworkId={selectedArtworkId}
-          />
-        )}
       <button
         className="btn btn-outline-primary btn-lg mx-2"
         onClick={() => window.history.back()}
